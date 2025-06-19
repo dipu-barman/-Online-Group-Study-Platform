3d7d208a64b9aa8f61a1b9b55b48b5f29a658eb9
dipu-barman <dipub5522@gmail.com>
2025-06-19 15:09:37 +0600

    added-sercure-data

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,13 +7,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjt0cAqJR4DRalL73G8uGf8ncgLT00PT0",
-  authDomain: "assignment-11-41e71.firebaseapp.com",
-  projectId: "assignment-11-41e71",
-  storageBucket: "assignment-11-41e71.firebasestorage.app",
-  messagingSenderId: "1063672553767",
-  appId: "1:1063672553767:web:48311694e3ee4dc80c4902"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
